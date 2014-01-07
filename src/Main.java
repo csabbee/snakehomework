f0bd5c999561978ee46146f33e62acd03787d3a3
Csaba Valyi <csaba_valyi@epam.com>
2014-01-07 16:23:18 +0100

    init commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/*
+ * A main() függvény.
+ * Létrehoz egy SNake objektumot, mely a program alapköve.
+ */
+
+public class Main {
+
+	public static void main(String[] args) {
+		new Snake();
+	}
+
+}
