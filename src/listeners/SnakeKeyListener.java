563b9240a373f62e3d8f8764bd1d044303edbcd4
Csaba Valyi <csaba_valyi@epam.com>
2014-01-08 14:34:30 +0100

    SnakeKeyListener introduced

---
+++ src/listeners/SnakeKeyListener.java
@@ -0,0 +1,29 @@
+package listeners;
+import game.SnakeInterface;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class SnakeKeyListener implements KeyListener{
+
+    private SnakeInterface snake;
+    
+    
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        snake.keyPressed(e.getKeyCode());
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+    }
+
+    public void setSnake(SnakeInterface snake) {
+        this.snake = snake;
+    }
+
+}
