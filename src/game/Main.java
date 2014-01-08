15f83c81138cdedada464ecdd6bbb87a80e3f3d3
Csaba Valyi <csaba_valyi@epam.com>
2014-01-08 14:34:50 +0100

    reconfig of the app

--- src/game/Main.java
+++ src/game/Main.java
@@ -1,5 +1,8 @@
 package game;
 
+import iooperations.FileHandler;
+import listeners.SnakeKeyListener;
+
 /*
  * A main() f�ggv�ny.
  * L�trehoz egy SNake objektumot, mely a program alapk�ve.
@@ -8,7 +11,11 @@ package game;
 public class Main {
 
 	public static void main(String[] args) {
-		new Snake();
+	    SnakeKeyListener snakeKeyListener = new SnakeKeyListener();
+	    FileHandler fileHandler = new FileHandler();
+	    
+		Snake snake = new Snake(snakeKeyListener, fileHandler);
+		snakeKeyListener.setSnake(snake);
 	}
 
 }
