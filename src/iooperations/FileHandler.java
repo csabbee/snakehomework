70e4287db1ceadbed6ae4ec67b2567ce49ec2f61
Csaba Valyi <csaba_valyi@epam.com>
2014-01-08 09:29:13 +0100

    handling the toplist in a more mannered way

--- src/iooperations/FileHandler.java
+++ src/iooperations/FileHandler.java
@@ -1,7 +1,5 @@
 package iooperations;
 
-import game.Snake;
-
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
 import java.io.FileInputStream;
@@ -14,26 +12,25 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
+import java.util.List;
 
 import toplist.Toplist;
 
 public class FileHandler {
 
     @SuppressWarnings("unchecked")
-    public
-    void fajlmegnyitas(Snake snake) {
-    	// A f�jl megnyit�sa
+    public void fajlmegnyitas(List<Toplist> list) {
     	try {
     		InputStream file = new FileInputStream("toplista.ser");
     		InputStream buffer = new BufferedInputStream(file);
     		ObjectInput in;
     		in = new ObjectInputStream(buffer);
+    		
+    		List<Toplist> fromFile = (List<Toplist>) in.readObject();
+    		for (Toplist toplist : fromFile) {
+                list.add(toplist);
+            }
     
-    		// A f�jl tartalm�nak bem�sol�sa a lista ArrayListbe
-    		snake.lista = (ArrayList<Toplist>) in.readObject();
-    
-    		// A f�jl bez�r�sa
     		in.close();
     	} catch (FileNotFoundException e) {
     		e.printStackTrace();
@@ -44,19 +41,16 @@ public class FileHandler {
     	}
     }
 
-    public void fajlbairas(Snake snake) {
-    	// A f�jl megnyit�sa
+    public void fajlbairas(List<Toplist> lista) {
     	try {
     		OutputStream file = new FileOutputStream("toplista.ser");
     
     		OutputStream buffer = new BufferedOutputStream(file);
     		ObjectOutput out;
     		out = new ObjectOutputStream(buffer);
+
+    		out.writeObject(lista);
     
-    		// A lista ArrayList f�jlba �r�sa
-    		out.writeObject(snake.lista);
-    
-    		// A f�jl bez�r�sa
     		out.close();
     	} catch (FileNotFoundException e) {
     		e.printStackTrace();
