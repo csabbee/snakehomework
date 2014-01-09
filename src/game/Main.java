349e81f70cc78deadbf561f5ce29cc93fad085c3
Csaba Valyi <csaba_valyi@epam.com>
2014-01-09 12:20:56 +0100

    wiring together the menus

--- src/game/Main.java
+++ src/game/Main.java
@@ -2,6 +2,8 @@ package game;
 
 import iooperations.FileHandler;
 import listeners.SnakeKeyListener;
+import menu.Menu;
+import menu.MenuBar;
 
 public class Main {
 
@@ -9,8 +11,16 @@ public class Main {
 	    SnakeKeyListener snakeKeyListener = new SnakeKeyListener();
 	    FileHandler fileHandler = new FileHandler();
 	    
+	    Menu menu = new Menu();
+	    MenuBar menuBar = new MenuBar();	    
 		Snake snake = new Snake(snakeKeyListener, fileHandler);
+		
+		menu.setSnake(snake);
+		menuBar.addMenus(menu.getMenus());
+		snake.setJMenuBar(menuBar);
+		
 		snakeKeyListener.setSnake(snake);
+		snake.start();
 	}
 
 }
